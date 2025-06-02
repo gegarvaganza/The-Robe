bd64ad9fa266f55c7551d960596b354991699768
gegarvaganza <ahmadhakim305@gmail.com>
2025-06-02 19:51:14 +0800

    MAKING SFX

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [Header("--------------- Audio Source ----------------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------------- Audio Clip ----------------")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip wallTouch;
+    public AudioClip Jump;
+    public AudioClip Crouch;
+    public AudioClip Run;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    private void PlaySFX(AudioClip clip)
+    {
+
+    }
+}
